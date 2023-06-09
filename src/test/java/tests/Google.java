8a756178b4bac6ad948525dab513933f3cf0d590
Nurlan1 <tashpolotovnurlan@mail.ru>
2023-06-08 21:24:21 -0500

    Added driver and quit it

--- src/test/java/tests/Google.java
+++ src/test/java/tests/Google.java
@@ -2,10 +2,12 @@ package tests;
 
 import org.junit.Assert;
 import org.junit.Test;
+import utilites.Driver;
 
 public class Google {
     @Test
     public void google(){
-        Assert.assertTrue(true);
+        Driver.getDriver().get("https://www.google.com/");
+        Driver.quit();
     }
 }
