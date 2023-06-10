71dfd253d4252442f59111bae3c99d3749b378bf
nurlan4 <mr.tashpolotov@gmail.com>
2023-06-09 19:46:45 -0500

    Removed Sysout from googleImage

--- src/test/java/tests/Google.java
+++ src/test/java/tests/Google.java
@@ -13,7 +13,6 @@ public class Google {
     @Test
     public void googleImage(){
         Driver.getDriver().get("https://www.google.com/");
-        System.out.println("Google images");
         Driver.quit();
     }
 }
