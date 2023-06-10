51926edd8219983b87b975ae88c3b1152f8b75e2
nurlan4 <mr.tashpolotov@gmail.com>
2023-06-09 20:44:42 -0500

    Added amazon pages

---
+++ src/test/java/pages/AmazonHomePage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilites.Driver;
+
+public class AmazonHomePage {
+    public AmazonHomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+    @FindBy(id="something")
+    public WebElement login;
+
+}
